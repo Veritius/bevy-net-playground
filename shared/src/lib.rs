0f8f3091c8f5a6ad44eb07b81e3bcadcd59c7bad
Veritius <veritiusgaming@gmail.com>
2023-05-25 14:47:05 +1000

    Add game version static

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,1 +1,11 @@
+pub use bevy;
+pub use naia_bevy_shared;
+pub use semver;
+
 pub mod network;
+
+use bevy::render::once_cell::sync::Lazy;
+use semver::Version;
+
+/// The version of the game.
+pub static GAME_VERSION: Lazy<Version> = Lazy::new(|| Version::parse(env!("CARGO_PKG_VERSION")).unwrap());
