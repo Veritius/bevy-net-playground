c1deaec7b405efc1472ab96906fd8999029dd4d7
Veritius <veritiusgaming@gmail.com>
2023-05-25 15:46:50 +1000

    Basic client and server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,14 @@
+use naia_bevy_client::{Plugin as ClientPlugin, ClientConfig};
+use playground_shared::{bevy::prelude::*, network::protocol};
+
 fn main() {
-    
+    let mut app = App::default();
+
+    // Add default plugins
+    app.add_plugins(DefaultPlugins);
+
+    // Set up naia client
+    app.add_plugin(ClientPlugin::new(ClientConfig::default(), protocol()));
+
+    app.run();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
+use naia_bevy_server::{Plugin as ServerPlugin, ServerConfig};
+use playground_shared::{bevy::prelude::*, network::protocol};
+
 fn main() {
-    
+    let mut app = App::default();
+
+    // Add default plugins
+    app.add_plugins(DefaultPlugins);
+
+    // Set up naia server
+    app.add_plugin(ServerPlugin::new(ServerConfig::default(), protocol()));
+
+    app.run();
 }
