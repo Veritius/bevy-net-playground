e02af0e242597fd42460914bb206396e0e2a24eb
Veritius <veritiusgaming@gmail.com>
2023-05-26 12:00:29 +1000

    Better comments and logging in server events handling

--- server/src/network/events.rs
+++ server/src/network/events.rs
@@ -1,25 +1,34 @@
 use naia_bevy_server::{Server, events::{AuthEvents, ConnectEvent, DisconnectEvent}};
 use playground_shared::{bevy::prelude::*, network::auth::AuthMessage, version::GAME_VERSION};
 use crate::network::entity::NetworkUserEntity;
-
 use super::room::HubRoom;
 
+/// Loops through all clients trying to connect and accepts/denies them based on information like game version.
 pub(super) fn auth_events(
     mut server: Server,
     mut events: EventReader<AuthEvents>,
 ) {
     for events in events.iter() {
         for (user_key, auth) in events.read::<AuthMessage>() {
+            // Get address for logging
+            let address = server.user(&user_key).address();
+
             // Check client version
             let client_version = playground_shared::semver::Version::parse(&auth.version);
-            if client_version.is_err() || client_version.unwrap() != *GAME_VERSION { server.reject_connection(&user_key); return; }
+            if client_version.is_err() || client_version.unwrap() != *GAME_VERSION {
+                server.reject_connection(&user_key);
+                info!("Client at address {} was rejected for invalid version: {}", &auth.version, &address);
+                continue;
+            }
 
             // All checks succeeded
             server.accept_connection(&user_key);
+            info!("Client at address {} was accepted", &address);
         }
     }
 }
 
+/// Logs users that have connected and assigns them entities.
 pub(super) fn connect_events(
     mut commands: Commands,
     mut server: Server,
@@ -38,6 +47,7 @@ pub(super) fn connect_events(
     }
 }
 
+/// Logs disconnected users and despawns their assigned entity.
 pub(super) fn disconnect_events(
     mut commands: Commands,
     mut events: EventReader<DisconnectEvent>,
