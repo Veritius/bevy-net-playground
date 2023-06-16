c196b3d1f656ac17565c55ae9d51b30bf1e2e185
Veritius <veritiusgaming@gmail.com>
2023-06-16 13:19:17 +1000

    Implement Serde for quats

--- shared/src/protocol/bevy.rs
+++ shared/src/protocol/bevy.rs
@@ -78,3 +78,37 @@ impl Serde for ExternalWrapper<Vec3> {
         32*3
     }
 }
+
+impl Serde for ExternalWrapper<Quat> {
+    fn ser(&self, writer: &mut dyn BitWrite) {
+        let v = self.0.to_array();
+        for i in 0..=3 {
+            writer.write_bits(v[i].to_bits());
+        }
+    }
+
+    fn de(reader: &mut BitReader) -> Result<Self, SerdeErr> {
+        let mut v = [[0u8; 4]; 4];
+
+        for i in 0..=4*4 {
+            let byte = reader.read_byte();
+            if byte.is_err() { return Err(byte.unwrap_err()) }
+            let z = i / 4;
+            let y = i % 4;
+            v[z][y] = byte.unwrap();
+        }
+
+        let x = f32::from_ne_bytes(v[0]);
+        let y = f32::from_ne_bytes(v[1]);
+        let z = f32::from_ne_bytes(v[2]);
+        let w = f32::from_ne_bytes(v[3]);
+
+        let quat = Quat::from_array([x, y, z, w]);
+
+        return Ok(Self(quat));
+    }
+
+    fn bit_length(&self) -> u32 {
+        32*4
+    }
+}
