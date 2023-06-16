3f36658cb409020917d85423a6060fc802079e26
Veritius <veritiusgaming@gmail.com>
2023-06-16 14:04:04 +1000

    Implement Serde for Color and Transform

--- shared/src/protocol/bevy.rs
+++ shared/src/protocol/bevy.rs
@@ -122,12 +122,73 @@ impl Serde for ExternalWrapper<Quat> {
             v[i] = f32::from_bits(bits.unwrap());
         }
 
-        let vec = Quat::from_array(v);
+        let quat = Quat::from_array(v);
 
-        return Ok(Self(vec));
+        return Ok(Self(quat));
     }
 
     fn bit_length(&self) -> u32 {
         32*4
+    }
+}
+
+impl Serde for ExternalWrapper<Color> {
+    fn ser(&self, writer: &mut dyn BitWrite) {
+        let v = self.0.as_linear_rgba_f32();
+        for x in v {
+            writer.write_bits(x.to_bits());
+        }
+    }
+
+    fn de(reader: &mut BitReader) -> Result<Self, SerdeErr> {
+        const LENGTH: usize = 4;
+
+        let mut v = [0.0f32; LENGTH];
+
+        for i in 0..=LENGTH {
+            let bits = reader.read_bits();
+            if bits.is_err() { return Err(bits.unwrap_err()) }
+            v[i] = f32::from_bits(bits.unwrap());
+        }
+
+        let color = Color::rgba(v[0], v[1], v[2], v[3]);
+
+        return Ok(Self(color));
+    }
+
+    fn bit_length(&self) -> u32 {
+        32*4
+    }
+}
+
+impl Serde for ExternalWrapper<Transform> {
+    fn ser(&self, writer: &mut dyn BitWrite) {
+        let pos = ExternalWrapper(self.0.translation);
+        let rot = ExternalWrapper(self.0.rotation);
+        let sca = ExternalWrapper(self.0.scale);
+
+        pos.ser(writer);
+        rot.ser(writer);
+        sca.ser(writer);
+    }
+
+    fn de(reader: &mut BitReader) -> Result<Self, SerdeErr> {
+        let pos = ExternalWrapper::<Vec3>::de(reader);
+        let rot = ExternalWrapper::<Quat>::de(reader);
+        let sca = ExternalWrapper::<Vec3>::de(reader);
+        
+        if pos.is_err() | rot.is_err() | sca.is_err() {
+            return Err(SerdeErr)
+        }
+
+        Ok(ExternalWrapper(Transform {
+            translation: pos.unwrap().0,
+            rotation: rot.unwrap().0,
+            scale: sca.unwrap().0
+        }))
+    }
+
+    fn bit_length(&self) -> u32 {
+        32*(6+4)
     }
 }
